c76d6336d122c5ff4e61735a01c4c7940ae43948
erikmmkarlsson <erik.karlsson97@outlook.com>
2021-05-29 01:52:23 +0200

    created modules for the different parts of the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <>
       <div className="App">
-     
+   
+      <Topbar/>
 
       </div>
-
     </>
   );
 }
