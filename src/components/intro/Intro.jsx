30cc0236fa6fe72a4ac67f09a4f10562b01e2c2a
erikmmkarlsson <erik.karlsson97@outlook.com>
2021-06-02 01:31:20 +0200

    Added intro picture and rectangle across site

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,16 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left"> 
+            <div className="colorBox"></div>
+            <div className="imgContainer">
+                <img src="assets\DSC_0684.jpg" alt="" />
+            </div>
+            </div>
+
+            <div className="right"> 
             
+            </div>
         </div>
     )
 }
