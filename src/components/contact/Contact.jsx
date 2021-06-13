be60a737bc992802f2b371bde41a677d2c0bded9
erikmmkarlsson <erik.karlsson97@outlook.com>
2021-06-13 15:12:46 +0200

    Changed contact css to be better

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,23 +5,22 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             
-            <div className="wrapper">
 
             <h1>Get in touch.</h1>
-            <div className="buttonContainer">
 
             <a href="mailto:erik.karlsson97@outlook.com" className="button">Say hi</a>
 
-            
+            <div className="mobileButtons">
             <a href="https://github.com/Erikmmkarlsson" target="blank">
-                    <GitHub className="icon"/>
-            </a>
+                    <GitHub className="icon"/></a>
+
                 <a href="https://www.linkedin.com/in/erik-karlsson-8643a1143/" target="blank">
-                    <LinkedIn className="icon"/> 
-            </a>
+                    <LinkedIn className="icon"/> </a>
             </div>
 
+
+
             </div>
-        </div>
+
     )
 }
